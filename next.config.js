9017a02e9d150aecc696aa23f8a5f4c43524d261
MCSEDU <janet.wu@vadcoordinator.org>
2026-03-27 05:53:39 -0700

    Add next.config.js for LVAD Education project

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  reactStrictMode: true,
+  images: {
+    domains: ['example.com'], // Add your image domains here
+  },
+  env: {
+    API_URL: 'https://api.example.com', // Replace with your API URL
+  },
+};
